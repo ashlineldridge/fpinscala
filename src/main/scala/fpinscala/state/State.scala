849361f1bf5fb5fcce62a6814f28362bdcd10c50
Ashlin Eldridge <aeldridge@seek.com.au>
2016-09-29 11:52:24 +1000

    State: added sequence variations, flatMap, etc

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -35,10 +35,15 @@ object RNG {
     (if (i < 0) -(i + 1) else i, r)
   }
 
+  /** Returns a double in the range [0,1) */
   def double(rng: RNG): (Double, RNG) = {
     val (i, r) = nonNegativeInt(rng)
-    (i.toDouble / (Int.MaxValue + 1), r)
+    (i / (Int.MaxValue.toDouble + 1), r)
   }
+
+  /** Reimplmentation of double via map - using `Rand` in sig to clean things up */
+  def doubleViaMap: Rand[Double] =
+    map(nonNegativeInt)(_/ (Int.MaxValue.toDouble + 1))
 
   def intDouble(rng: RNG): ((Int,Double), RNG) = {
     val (i, r1) = rng.nextInt
@@ -69,11 +74,50 @@ object RNG {
     go(List(), rng)
   }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      (f(a, b), r2)
+    }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_, _))
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((a, as) => map2(a, as)(_ :: _))
+
+  def sequenceRecursive[A](fs: List[Rand[A]]): Rand[List[A]] = fs match {
+    case h :: t => map2(h, sequence(t))(_ :: _)
+    case _ => unit(List[A]())
+  }
+
+  def sequenceTailRecursive[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    @tailrec
+    def go(rs: Rand[List[A]], xs: List[Rand[A]]): Rand[List[A]] = xs match {
+      case h :: t => go(map2(h, rs)(_ :: _), t)
+      case _      => rs
+    }
+    go(unit(List[A]()), fs)
+  }
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, r1) = f(rng)
+    g(a)(r1)
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+
+  def mapViaFlatmap[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2ViaFlatmap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+
 }
 
 case class State[S,+A](run: S => (A, S)) {
